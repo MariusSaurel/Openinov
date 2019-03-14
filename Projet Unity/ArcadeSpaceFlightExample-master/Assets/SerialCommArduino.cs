2fdb005f1368b70dd90969af5d11c2163e19a1c9
Marius Saurel <marius.saurel@hotmail.fr>
2019-03-14 20:21:10 +0100

    las version apres test

---
+++ Projet Unity/ArcadeSpaceFlightExample-master/Assets/SerialCommArduino.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class SerialCommArduino : MonoBehaviour
+{
+    SerialPort arduino;
+    float rotationx;
+    float rotationy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arduino = new SerialPort("COM5", 9600);
+        arduino.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (arduino.IsOpen)
+        {
+                arduino.Write(rotationx + "|" + rotationy );
+                Debug.Log(1);
+        }
+    }
+}
